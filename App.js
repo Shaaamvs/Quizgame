a23b1d961b7c6466a2330614e59f0a6a150e0b44
Shaaamvs <130343285+Shaaamvs@users.noreply.github.com>
2023-09-16 04:59:18 -0700

    Update App.js

--- App.js
+++ App.js
@@ -6,10 +6,10 @@ function Quiz() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [selectedAnswers, setSelectedAnswers] = useState([]);
   const [score, setScore] = useState(0);
-  const [timer, setTimer] = useState(10); // Timer in seconds
+  const [timer, setTimer] = useState(10); 
 
   useEffect(() => {
-    // Fetch questions and answer choices from the API
+  
     axios.get('https://api.punkapi.com/v2/beers')
       .then(response => {
         const fetchedQuestions = response.data.slice(0, 10);
@@ -22,7 +22,7 @@ function Quiz() {
   }, []);
 
   useEffect(() => {
-    // Set up a timer for each question
+  
     if (timer > 0) {
       const timerInterval = setInterval(() => {
         setTimer(prevTimer => prevTimer - 1);
@@ -30,8 +30,7 @@ function Quiz() {
 
       return () => clearInterval(timerInterval);
     } else {
-      // Auto-submit the answer when the timer expires
-      handleSubmitAnswer();
+          handleSubmitAnswer();
     }
   }, [timer]);
 
@@ -56,12 +55,10 @@ function Quiz() {
 
     if (currentQuestion < questions.length - 1) {
       setCurrentQuestion(prevQuestion => prevQuestion + 1);
-      setSelectedAnswers([]); // Reset selected answers
-      setTimer(10); // Reset timer for the next question
+      setSelectedAnswers([]);
+      setTimer(10); 
     } else {
-      // End of the quiz
-      // Display the final score or any other completion logic
-    }
+        }
   };
 
   const currentQuestionData = questions[currentQuestion];
@@ -74,11 +71,13 @@ function Quiz() {
 
   return (
     <div className='container text-center'>
-      <h3 className='display-4'>Quiz App</h3>
+      <h3 className='btn btn-success'> <p className='display-4'> Quiz Game!....
+        </p></h3>
+      <br/>
       {currentQuestionData && (
         <div>
-          <h2 className="display-5">Question {currentQuestion + 1}</h2>
-          <p className='h3'>what is {currentQuestionData.name}</p>
+          <h2 className="display-5" >Question {currentQuestion + 1}</h2>
+          <p  type="button" className="btn btn-dark h1" >what is {currentQuestionData.name}</p>
           <ul className="list-group">
             {choices.map((choice, index) => (
               <li className="list-group-item"
@@ -97,7 +96,7 @@ function Quiz() {
           </ul>
           <br/>
           <div>
-            <p>Time Remaining: {timer} seconds</p>
+            <p className='h5'>Time Remaining: <span type="button" className="btn btn-danger mx-3">{timer} seconds </span></p>
             <button onClick={handleSubmitAnswer}
             type="button" className="btn btn-primary">Submit Answer</button>
 
@@ -105,8 +104,10 @@ function Quiz() {
         </div>
       )}
       <div>
+        <br/>
         <h2>Final Score</h2>
-        <p>{score}</p>
+        <br/>
+        <h1 type="button" className="btn btn-success">{score}</h1>
       </div>
     </div>
   );
